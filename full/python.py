8be0f143bbb925205859c2de54c9de3b1c76cae1
JeremiasPiontkoski <jeremiaspion@gmail.com>
2024-03-18 14:42:57 -0300

    Add functions time

--- full/python.py
+++ full/python.py
@@ -12,6 +12,9 @@ start_time = time.time()
 
 #Filtro Bandpass
 def band_filter(signal):
+  global band_filter_time
+  initial = time.time()
+  
   for cont_sin in range(len(signal)):
 
     #Band pass Filter
@@ -66,10 +69,15 @@ def band_filter(signal):
     #print("AQUI ESTA O MAX VAL: ",max_val)
     result = result/max_val
 
+    final = time.time()
+    band_filter_time = final - initial
+
     return result
 
 #Extrair Características Temporais
 def carac_temp(df):
+  global carac_temp_time
+  initial = time.time()
 
   vet_pre = []
   vet_post = []
@@ -115,11 +123,17 @@ def carac_temp(df):
   avg = temp_sum/(len(df)-1)
 
   df["glob"] = avg
+  
+  final = time.time()
+  carac_temp_time = final - initial
 
   return df
 
 #Extrair características morofológicas
 def carac_morf(df):
+  global carac_morf_time
+  initial = time.time()
+  
   coef_max = []
   coef_min = []
   coef_mean = []
@@ -143,10 +157,16 @@ def carac_morf(df):
   df['morf_mean'] =  coef_mean
   df['morf_stdr_dev']  = coef_stndr_dev
 
+  final = time.time()
+  carac_morf_time = final - initial
+
   return df
 
 #Dataset para vetores Numpy
 def adeq_dts(DF_MLII,DF_V5):
+  global adeq_dts_time
+  initial = time.time()
+  
   colunas = ["pre", "post", "loc", "glob", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100", "101", "102", "103", "104", "105", "106", "107", "108", "109", "110", "111", "112", "113", "114", "115", "116", "117", "118", "119", "120", "121", "122", "123", "124", "125", "126", "127", "128", "129", "130", "131", "132", "133", "134", "135", "136", "137", "138", "139", "140", "141", "142", "143", "144", "145", "146", "147", "148", "149", "150", "151", "152", "153", "154", "155", "156", "157", "158", "159", "160", "161", "162", "163", "164", "165", "166", "167", "168", "169", "170", "171", "172", "173", "174", "175", "176", "177", "178", "179", "180", "181", "182", "183", "184", "185", "186", "187", "188", "189", "190", "191", "192", "193", "194", "195", "196", "197", "198", "199", "200", "201", "202", "203", "204", "205", "206", "207", "208", "209", "210", "211", "212", "213", "214", "215", "216", "217", "218", "219", "220", "221", "222", "223", "224", "225", "226", "227", "228", "229", "230", "231", "232", "233", "234", "235", "236", "237", "238", "239", "240", "241", "242", "243", "244", "245", "246", "247", "248", "249", "250", "251", "252", "morf_max",	"morf_min",	"morf_mean",	"morf_stdr_dev"]
 
   #Convertendo para string colunas DF_MLII
@@ -177,11 +197,16 @@ def adeq_dts(DF_MLII,DF_V5):
   for index in range(len(DF_V5)):
       base_V5.append(DF_V5.iloc[index].values)
 
+  final = time.time()
+  adeq_dts_time = final - initial
 
   return base_MLII,base_V5
 
 #Traduzindo predição realizada
 def tpSig_predc(predc):
+  global tpSig_predc_time
+  initial = time.time()
+  
   carac_predic = []
   for index in range(len(predc)):
     temp_vet = predc[index].tolist()
@@ -198,8 +223,14 @@ def tpSig_predc(predc):
     elif(temp_var == 4):
       carac_predic.append("V")
     else: print("ERRO")
+  
+  final = time.time() 
+  tpSig_predc_time = final - initial  
+  
   return carac_predic
 
+global extraTime
+initialExtra = time.time()
 
 #Importando dados sinal MIT para DF
 df = pd.read_csv("200.csv",sep=",")
@@ -332,8 +363,30 @@ predic = model.predict([base_MLII,base_V5])
 #Identificando batida através de predição
 vetor_final_predic = tpSig_predc(predic)
 
+finalExtra = time.time()
 end_time = time.time()
 
-final_time = end_time - start_time
+#Tempo do código fora das funções executar
+extraTime = finalExtra - initialExtra
 
-print(final_time)
+def getTotalTimeFunctions():
+  sum_time_functions = band_filter_time + carac_morf_time + carac_temp_time + adeq_dts_time + tpSig_predc_time + extraTime
+  print(f'Tempo de todas as funções executarem: {sum_time_functions}')
+  
+def getTotalTimeCode():
+  #Tempo de todo o código executar
+  final_time = end_time - start_time
+  print(f'Tempo de todo o código executar: {final_time}')
+  
+def getTotalTimeByFunction():
+  print(f'band_filter: {band_filter_time}')
+  print(f'carac_temp: {carac_temp_time}')
+  print(f'carac_morf: {carac_morf_time}')
+  print(f'adeq_dts: {adeq_dts_time}')
+  print(f'tpSig_predc: {tpSig_predc_time}')
+  print(f'Código restante: {extraTime}')
+  getTotalTimeFunctions()
+
+  
+getTotalTimeByFunction()
+getTotalTimeCode()
